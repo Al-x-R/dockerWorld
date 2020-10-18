b639ea823cdf1cb815faf57f7c4fcc354dc1fb06
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-10-18 17:54:36 +0300

    Refactor to prod

--- api/src/helpers/db.js
+++ api/src/helpers/db.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const {db} = require('./../configs/index');
 
 module.exports.connectDb = () => {
-    mongoose.connect(db, {useNewUrlParser: true});
+    mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true});
     return mongoose.connection;
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <strong>Hello from docker</strong>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const makeApiRequest = async () => {
+        const request = await axios.get('/api/testcurrentuser');
+        console.log(request);
+    };
+
+    return (
+
+
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <strong>Hello from docker</strong>
+                <button onClick={makeApiRequest}>Make api request</button>
+
+            </header>
+        </div>
+    );
 }
 
 export default App;
